31ba426d133551bea2ba805d2764f94d628e6c8b
Matheus Tulio <matheus.tulio@cognizant.com>
2020-09-19 01:30:16 -0300

    JsonPath e XML

--- src/main/java/br/com/edsoft/JsonPathTest.java
+++ src/main/java/br/com/edsoft/JsonPathTest.java
@@ -3,20 +3,11 @@ package br.com.edsoft;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.request;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anyOf;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.endsWith;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
-import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.*;
+
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,7 +21,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
-public class OlaMundoTest {
+public class JsonPathTest {
 
 	@Test
 	public void olaMundo() {
@@ -109,11 +100,101 @@ public class OlaMundoTest {
 		.body("id", is(2))
 		.body("name", containsString("Joaquina"))
 		.body("endereco.rua", is("Rua dos bobos"));
-
-		
 	}
+    
+    
+    @Test
+    public void deveVerificarTerceiroNivel() {
+    	given().
+		when().
+		get("https://restapi.wcaquino.me/users/3")
+		.then()
+		.statusCode(200)
+		.body("id", is(3))
+		.body("name", containsString("Ana"))
+		.body("filhos", hasSize(2))
+		.body("filhos[0].name", is("Zezinho"))
+		.body("filhos[1].name", is("Luizinho"))
+		.body("filhos.name", hasItem("Zezinho"))
+		.body("filhos.name", hasItems("Zezinho", "Luizinho"))
+		;
+    }
+
+    @Test
+    public void deveRetornaErroUsuarioInesxistente() {
+    	given().
+    	when().
+    	get("https://restapi.wcaquino.me/users/4")
+    	.then()
+    	.statusCode(404)
+        .body("error", is("Usuário inexistente"))
+    ;
+    }
+
+    @Test
+    public void deveVerificarListaRaiz() {
+    	given().
+    	when().
+    	get("https://restapi.wcaquino.me/users")
+    	.then()
+    	.statusCode(200)
+    	.body("$", hasSize(3))
+    	.body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
+    	.body("filhos.name", hasItem(Arrays.asList("Zezinho","Luzinho")))
+    	.body("salary", contains(1234.5678f, 2500, null));
+    }
+
+    @Test
+    public void deveFazerVerificoesAvancadas() {
+    	given().
+    	when().
+    	get("https://restapi.wcaquino.me/users")
+    	.then()
+    	.statusCode(200)
+    	.body("$", hasSize(3))
+        .body("age.findAll{it <= 25}.size()", is(2))    	
+        .body("age.findAll{it <= 25 && it > 20}.size()", is(1))    	
+        .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))    	
+        .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))    	
+        .body("find{it.age <= 25}.name", is("Maria Joaquina"))
+        .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina","Ana Júlia"))
+        .body("findAll{it.name.length()>10}.name", hasItems("João da Silva","Maria Joaquina"))
+        .body("name.collect{it.toUpperCase()}",hasItem("MARIA JOAQUINA"))
+        .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}",hasItem("MARIA JOAQUINA"))
+        .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()",allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+        .body("age.collect{it * 2}", hasItems(60,50,40))    	
+        .body("id.max()", is(3))
+        .body("salary.min()", is(1234.5678f))
+        .body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f,0.001)))
+        .body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)))
+        
+        
+    	;
+    }
+
+    @Test
+    public void devoUnirJsonPathComJava() {
+    	ArrayList<String> names = 
+    	
+    	given().
+    	when().
+    	get("https://restapi.wcaquino.me/users")
+    	.then()
+    	.statusCode(200)
+    	.body("$", hasSize(3))
+        .extract().path("name.findAll{it.startsWith('Maria')}")    	
+    	
+    	
+    	;
+    	Assert.assertEquals(1, names.size());
+    	Assert.assertTrue(names.get(0).equalsIgnoreCase("Maria Joaquina"));
+    	Assert.assertEquals(names.get(0).toUpperCase(),"maria joaquina".toUpperCase());
+    	
+    }
 	
-	
+
+    
+    
 	
 	
 	
